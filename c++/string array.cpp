6a468a4b4f85689aa3ddca217489e02835e90174
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-10-09 17:01:27 +0530

    Update string array.cpp

--- c++/string array.cpp
+++ c++/string array.cpp
@@ -1,14 +1,14 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
 using namespace std;
-void myfunction(int mark[6],std::string stlstrings[]) //declare a function
+void myfunction(int mark[6],string stlstrings[]) //declare a function
 {   
     cout<<"Display mark:"<<endl;
     for(int i=0; i<6; i++)
@@ -19,7 +19,7 @@ void myfunction(int mark[6],std::string stlstrings[]) //declare a function
 int main()
 { 
     int mark[6]={88, 85, 45, 75, 90, 100}; //declare & initialize array
-    std::string stlstrings[]{"uniform", "victor", "whiskey", "x-ray", "yankee", "zulu"}; 
+    string stlstrings[]{"uniform", "victor", "whiskey", "x-ray", "yankee", "zulu"}; 
     myfunction(mark,stlstrings);
     return 0;
 }
