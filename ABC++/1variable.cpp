259976876d52740e86fa1bd2a1fdedc51143d928
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-10 18:08:24 +0530

    hello world

---
+++ ABC++/1variable.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c; //local variable
+    a=10; //initialization
+    b=20;
+    c=a+b;
+    cout<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c; //local variable
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"total sum is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a-b;
+    cout<<"total sub is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"total mul is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a/b;
+    cout<<"div is:"<<c;
+}
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"total sum is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"total sum is:"<<c;
+}
+ 
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a-b;
+    cout<<"sub:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"mul is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    c=a/b;
+    cout<<"div is:"<<c;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"pooja more";
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"A";
+    
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int id;
+    cout<<3;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double salary;
+    cout<<2500.23;
+}
+
