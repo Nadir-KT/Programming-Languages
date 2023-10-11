4beb071667c01b98edde6371fa6717a15ea4f559
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-11 18:27:38 +0530

    variable&operator

---
+++ ABC++/1.2operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=5, y=8;
+    ++x;
+    cout<<x<<endl;
+    --y;
+    cout<<y<<endl;
+}
---
+++ ABC++/2.2 variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c; //local variable
+    a=1.5; //initialization
+    b=3.5;
+    c=a+b;
+    cout<<c<<endl;
+    c=a-b;
+    cout<<c<<endl;
+    c=a*b;
+    cout<<c<<endl;
+    c=a/b;
+    cout<<c<<endl;
+}
+
