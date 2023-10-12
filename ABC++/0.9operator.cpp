5cb45ee05cc5039aa8968a99a98ba1139ec93f2b
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-12 12:02:27 +0530

    operator & variable

---
+++ ABC++/0.9operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=5, y=3; //initialize
+    cout<<(x>y)<<endl;
+    cout<<(x<y)<<endl;
+    cout<<(x==y)<<endl;
+    cout<<(x!=y)<<endl;
+    cout<<(x<=y)<<endl;
+    cout<<(x>=y)<<endl;
+}
