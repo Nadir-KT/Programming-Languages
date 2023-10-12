f7e2890ad51bfa861719176b0992683ccb3dc13b
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-12 18:24:40 +0530

    condition & loop

---
+++ ABC++/1.2concatenation.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string firstName = " arjun ";
+string lastName = " thorat ";
+string fullName = firstName.append(lastName);
+cout << fullName;
+}
