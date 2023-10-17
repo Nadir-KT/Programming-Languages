2a2b3d86c368ddf544b583fc71878a132a0b7a02
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-17 11:07:31 +0530

    comvariable

---
+++ c++/4.7combineallvariable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c; //local variable
+    a=10; //initialization
+    b=20;
+    c=a+b;
+    cout<<c<<endl;
+    c=a-b;
+    cout<<c<<endl;
+    c=a*b;
+    cout<<c<<endl;
+    c=a/b;
+    cout<<c;
+    float e, f, g; //local variable
+    e=1.5; //initialization
+    f=3.5;
+    g=e+f;
+    cout<<g<<endl;
+    g=e-f;
+    cout<<g<<endl;
+    g=e*f;
+    cout<<g<<endl;
+    g=e/f;
+    cout<<g<<endl;
+    string name="Pooja more";
+    cout<<name<<endl;
+    char ch;
+    cout<<"A";
+}
+
