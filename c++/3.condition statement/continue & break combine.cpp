26f3a47ce3e81035e5a41e14b632986a5d388873
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-18 11:18:45 +0530

    break

--- c++/3.condition statement/continue & break combine.cpp
+++ c++/3.condition statement/continue & break combine.cpp
@@ -1,20 +1,15 @@
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-     for(int i=1;i<=10;i++)
-     {      
-            if(i==2)
-             {      
-                break;      
-             }  
-             for(int j=1;j<=10;j++)
-            {      
-            if(j==4)
-             {      
-                break;      
-             }  
-            cout<<i<<" "<<j<<endl;
-             
-        }   }     
-}  
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num=5;
+    for(int i=1; i<=10; i++)
+    {
+        if(1)
+        {
+            cout<<"num"<<endl;
+            continue;
+            break;
+        }
+    }
+}
