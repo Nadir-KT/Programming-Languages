667cb14b9b799d656f3349bb1d47dad631e23bce
AartiBansod <146544898+AartiBansod@users.noreply.github.com>
2023-10-23 12:47:09 +0530

    Create 15.single inheritance.cpp

---
+++ c++/15.single inheritance.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Account //base class
+{
+    public:
+    float salary=6000;
+};
+class programmer:public Account //derived class
+    public:
+    float bonus=5000;
+};
+int main(void)
+{
+    programmer p1;
+    cout<<"salary: "<<p1.salary<<endl;
+    cout<<"Bonus: "<<p1.bonus<<endl;
+}
